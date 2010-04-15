445dc9ff221ed0e70c620abaa868cee79530587f
Julio Cesar Alonso <juliocesar.alonso@gmail.com>
2010-04-15 14:43:56 +0200

    mail

--- app/models/notifications.rb
+++ app/models/notifications.rb
@@ -1,8 +1,8 @@
 class Notifications < ActionMailer::Base
 
   def question(email_params, sent_at = Time.now)
-    subject "[Peganda Gestoría Online] " << email_params[:company]
-    recipients "juliocesar.alonso@gmail.com" # Perhaps your address here?
+    subject "[Nuevo Contacto en Peganda Gestoría Online] " << email_params[:company]
+    recipients "info@peganda.com" # Perhaps your address here?
     from email_params[:address]
     sent_on sent_at
 
